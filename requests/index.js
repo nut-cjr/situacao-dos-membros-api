ee0e9cf54d98051948ad20f80acc995b3635d153
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2022-05-20 15:40:41 -0300

    improve code quality

--- requests/index.js
+++ requests/index.js
@@ -1,208 +1,180 @@
-const { GraphQLClient } = require('graphql-request');
+const { GraphQLClient } = require('graphql-request')
 
-const { allCardsQuery, cardByIdQuery, getLabelsQuery } = require('../queries');
+const { allCardsQuery, cardByIdQuery, getLabelsQuery } = require('../queries')
 
 const {
-  deleteCardMutation,
-  moveCardToPhaseMutation,
-  generateUpdateFieldsValuesMutation,
-  updateLabelsMutation
-} = require('../mutations');
+    deleteCardMutation,
+    moveCardToPhaseMutation,
+    generateUpdateFieldsValuesMutation,
+    updateLabelsMutation,
+} = require('../mutations')
 
 const client = new GraphQLClient('https://api.pipefy.com/graphql', {
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + process.env.PIPEFY_API_TOKEN,
-  },
-});
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + process.env.PIPEFY_API_TOKEN,
+    },
+})
 
 function deleteAllCards() {
-  const query = allCardsQuery;
-  let variables = { pipeId: 301706843 };
+    const query = allCardsQuery
+    let variables = { pipeId: 301706843 }
 
-  client.request(query, variables).then((data) => {
-    const cards = data.allCards.edges;
+    client.request(query, variables).then((data) => {
+        const cards = data.allCards.edges
 
-    cards.forEach((card) => {
-      const mutation = deleteCardMutation;
-      variables = { cardId: card.node.id };
-      client.request(mutation, variables);
-    });
-  });
+        cards.forEach((card) => {
+            const mutation = deleteCardMutation
+            variables = { cardId: card.node.id }
+            client.request(mutation, variables)
+        })
+    })
 }
 
 async function deleteCard(cardId) {
-  const mutation = deleteCardMutation;
-  const variables = { cardId };
+    const mutation = deleteCardMutation
+    const variables = { cardId }
 
-  try {
-    await client.request(mutation, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    await client.request(mutation, variables)
 }
 
 async function moveCardToPhase(cardId, destinationPhaseId) {
-  const mutation = moveCardToPhaseMutation;
-  const variables = { cardId, destinationPhaseId };
+    const mutation = moveCardToPhaseMutation
+    const variables = { cardId, destinationPhaseId }
 
-  try {
-    await client.request(mutation, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    await client.request(mutation, variables)
 }
 
 async function getAllCards(pipeId) {
-  const query = allCardsQuery;
-  const variables = { pipeId };
-  let data;
+    const query = allCardsQuery
+    const variables = { pipeId }
 
-  try {
-    data = await client.request(query, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    const data = await client.request(query, variables)
 
-  return data.allCards.edges;
+    return data.allCards.edges
 }
 
-async function getCardId(pipeId, email) {
+async function getCardIdByEmail(pipeId, email) {
+    const getEmailField = (fields) =>
+        fields.filter((field) => field.name === 'Email da CJR')[0]
 
-  const getEmailField = (fields) => fields.filter(field => field.name === 'Email da CJR')[0];
-
-  const cards = await getAllCards(pipeId);
-  const card = cards.filter((card) => getEmailField(card.node.fields)?.value === email)[0];
-  const cardId = card.node.id;
-  return cardId;
+    const cards = await getAllCards(pipeId)
+    const card = cards.filter(
+        (card) => getEmailField(card.node.fields)?.value === email
+    )[0]
+    const cardId = card.node.id
+    return cardId
 }
 
 async function getCardData(cardId) {
-  const query = cardByIdQuery;
-  const variables = { cardId };
-  let data;
+    const query = cardByIdQuery
+    const variables = { cardId }
 
-  try {
-    data = await client.request(query, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    const data = await client.request(query, variables)
 
-  return data.card;
+    return data.card
 }
 
 async function getPipeLabels(pipeId) {
-  const query = getLabelsQuery;
-  const variables = { pipeId };
-  let data;
+    const query = getLabelsQuery
+    const variables = { pipeId }
 
-  try {
-    data = await client.request(query, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    const data = await client.request(query, variables)
 
-  return data.pipe.labels;
+    return data.pipe.labels
 }
 
 async function updateLabels(cardId, labelsIds) {
-  const mutation = updateLabelsMutation;
-  const variables = { cardId, labelsIds };
+    const mutation = updateLabelsMutation
+    const variables = { cardId, labelsIds }
 
-  try {
-    await client.request(mutation, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    await client.request(mutation, variables)
 }
 
-
 async function updateFieldsValues(cardId, intention, fields, labelsIds) {
-  let values;
-  switch (intention) {
-    case 'Folga':
-      values = `[{
+    let values
+    switch (intention) {
+        case 'Folga':
+            values = `[{
         fieldId: "data_de_in_cio_2",
         value: "${
-          fields.filter((field) => field.name === 'Data de início')[0].value
+            fields.filter((field) => field.name === 'Data de início')[0].value
         }"
     }, {
         fieldId: "data_de_retorno_2",
         value: "${
-          fields.filter((field) => field.name === 'Data de retorno')[0].value
+            fields.filter((field) => field.name === 'Data de retorno')[0].value
         }"
     }, {
         fieldId: "voc_falou_com_seu_gerente_e_ou_l_der_1",
         value: "${
-          fields.filter(
-            (field) => field.name === 'Você falou com seu gerente e/ou líder?'
-          )[0].value
+            fields.filter(
+                (field) =>
+                    field.name === 'Você falou com seu gerente e/ou líder?'
+            )[0].value
         }"
-    }]`;
-      break;
-    case 'Férias':
-      values = `[{
+    }]`
+            break
+        case 'Férias':
+            values = `[{
         fieldId: "data_de_in_cio",
         value: "${
-          fields.filter((field) => field.name === 'Data de início')[0].value
+            fields.filter((field) => field.name === 'Data de início')[0].value
         }"
     }, {
         fieldId: "data_de_retorno",
         value: "${
-          fields.filter((field) => field.name === 'Data de retorno')[0].value
+            fields.filter((field) => field.name === 'Data de retorno')[0].value
         }"
     }, {
         fieldId: "voc_falou_com_seu_gerente_e_ou_l_der",
         value: "${
-          fields.filter(
-            (field) => field.name === 'Você falou com seu gerente e/ou líder?'
-          )[0].value
+            fields.filter(
+                (field) =>
+                    field.name === 'Você falou com seu gerente e/ou líder?'
+            )[0].value
         }"
-    }]`;
-      break;
-    case 'Afastamento':
-      values = `[{
+    }]`
+            break
+        case 'Afastamento':
+            values = `[{
                 fieldId: "data_de_in_cio_1",
                 value: "${new Date().toLocaleDateString('pt-BR')}"
-            }]`;
-      break;
-    case 'Atualizar informações sobre estágio':
-      values = `[{
+            }]`
+            break
+        case 'Atualizar informações sobre estágio':
+            values = `[{
                 fieldId: "faz_est_gio",
                 value: "${
-                  fields.filter((field) => field.name === 'Faz estágio?')[0]
-                    .value
+                    fields.filter((field) => field.name === 'Faz estágio?')[0]
+                        .value
                 }"
-            }]`;
-      break;
-    case 'Atualizar informações sobre núcleo':
-      values = `[{
+            }]`
+            break
+        case 'Atualizar informações sobre núcleo':
+            values = `[{
         fieldId: "n_cleo",
         value: [${labelsIds}]
-      }]`;
-    break;
-    default:
-      break;
-  }
+      }]`
+            break
+        default:
+            break
+    }
 
-  const mutation = generateUpdateFieldsValuesMutation(values);
-  const variables = { cardId };
+    const mutation = generateUpdateFieldsValuesMutation(values)
+    const variables = { cardId }
 
-  try {
-    await client.request(mutation, variables);
-  } catch (error) {
-    throw new Error(error.message);
-  }
+    await client.request(mutation, variables)
 }
 
 module.exports = {
-  deleteAllCards,
-  deleteCard,
-  moveCardToPhase,
-  getCardId,
-  getCardData,
-  updateFieldsValues,
-  getPipeLabels,
-  updateLabels
-};
+    deleteAllCards,
+    deleteCard,
+    moveCardToPhase,
+    getCardIdByEmail,
+    getCardData,
+    updateFieldsValues,
+    getPipeLabels,
+    updateLabels,
+}
--- server.js
+++ server.js
@@ -1,80 +1,78 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 
 const {
-  deleteAllCards,
-  deleteCard,
-  moveCardToPhase,
-  getCardId,
-  getCardData,
-  updateFieldsValues,
-  getPipeLabels,
-  updateLabels
-} = require('./requests');
+    deleteAllCards,
+    deleteCard,
+    moveCardToPhase,
+    getCardIdByEmail,
+    getCardData,
+    updateFieldsValues,
+    getPipeLabels,
+    updateLabels,
+} = require('./requests')
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.set('trust proxy', 1);
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.set('trust proxy', 1)
 
-const phasesId = {
-  Folga: 310223785,
-  Férias: 310223778,
-  'Só para projetos': 310223771,
-  Afastamento: 310223786,
-  'Sair da empresa': 310547859,
-};
+const PHASES_ID = {
+    Folga: 310223785,
+    Férias: 310223778,
+    'Só para projetos': 310223771,
+    Afastamento: 310223786,
+    'Sair da empresa': 310547859,
+}
+
+const PIPE_ID = 301538945
 
 app.post('/solicitation', async (req, res) => {
-  try {
-    const solicitationCardId = req.body.data.card.id;
-    const { fields } = await getCardData(solicitationCardId);
+    try {
+        const solicitationCardId = req.body.data.card.id
+        const { fields } = await getCardData(solicitationCardId)
 
-    const email = fields.find((field) => field.name === 'Email CJR').value;
-    const intention = fields.find(
-      (field) => field.name === 'Qual sua intenção?'
-    ).value;
-    const intentionPhaseId = phasesId[intention];
+        const email = fields.find((field) => field.name === 'Email CJR').value
+        const intention = fields.find(
+            (field) => field.name === 'Qual sua intenção?'
+        ).value
+        const intentionPhaseId = PHASES_ID[intention]
 
-    const pipeId = 301538945;
-    let labelsIds = []
-    
-    if (intention === 'Atualizar informações sobre núcleo') {
-      const labels = await getPipeLabels(pipeId);
-      const { value } = fields.find(field => field.name === 'Núcleo(s)');
-      labelsIds = labels.filter(label => value.includes(label.name)).map(label => Number(label.id));
+        let labelsIds = []
+
+        if (intention === 'Atualizar informações sobre núcleo') {
+            const labels = await getPipeLabels(PIPE_ID)
+            const { value } = fields.find((field) => field.name === 'Núcleo(s)')
+            labelsIds = labels
+                .filter((label) => value.includes(label.name))
+                .map((label) => Number(label.id))
+        }
+
+        const cardId = await getCardIdByEmail(PIPE_ID, email)
+
+        if (intention === 'Só para projetos' || intention === 'Afastamento') {
+            await moveCardToPhase(cardId, intentionPhaseId)
+        }
+
+        if (intention !== 'Sair da empresa' && intention !== 'Só para projetos')
+            await updateFieldsValues(cardId, intention, fields, labelsIds)
+
+        if (intention === 'Atualizar informações sobre núcleo');
+        await updateLabels(cardId, labelsIds)
+    } catch (error) {
+        console.error(error)
     }
 
-    const cardId = await getCardId(pipeId, email);
-
-    if (
-      intention === 'Só para projetos' ||
-      intention === 'Afastamento'
-    ) {
-      await moveCardToPhase(cardId, intentionPhaseId);
-    }
-
-    if (intention !== 'Sair da empresa' && intention !== 'Só para projetos')
-      await updateFieldsValues(cardId, intention, fields, labelsIds);
-
-    if (intention === 'Atualizar informações sobre núcleo');
-      await updateLabels(cardId, labelsIds);
-    //await deleteCard(solicitationCardId);
-  } catch (error) {
-    console.error(error);
-  }
-
-  return res.status(200).json({ success: 'success' });
-});
-
+    return res.status(200).json({ success: 'success' })
+})
 
 app.get('/test', async (req, res) => {
-  console.log('teste');
-  return res.status(200).json({ success: 'success' });
-});
+    console.log('teste')
+    return res.status(200).json({ success: 'success' })
+})
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log('app listening on port 4000');
-});
+    console.log('app listening on port 4000')
+})
